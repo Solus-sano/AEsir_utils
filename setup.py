d891b583378fc7e5bf01c3461c900eef4718acaf
Solus-sano <1048092353@qq.com>
2024-12-04 19:16:37 +0800

    update

--- setup.py
+++ setup.py
@@ -8,20 +8,21 @@ setup(
     description="A brief description of your project",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/your_username/your_project",  # 替换为你的GitHub链接
-    packages=find_packages(),  # 自动查找所有包
+    url="https://github.com/Solus-sano/AEsir_utils",  
+    packages=find_packages(),  
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",  # 替换为你的许可证
+        "License :: OSI Approved :: MIT License",  
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',  # 指定支持的Python版本
-    install_requires=[       # 可选，列出依赖
-        # "numpy",  # 示例
+    python_requires='>=3.6', 
+    install_requires=[       
+        "numpy",
+        "matpl"
     ],
-    entry_points={
-        "console_scripts": [
-            "script1=your_project.script1:main",  # 替换为你的脚本入口
-        ],
-    },
+    # entry_points={
+    #     "console_scripts": [
+    #         "script1=your_project.script1:main",  
+    #     ],
+    # },
 )
